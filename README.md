f900b8d76e3398fca865444a93f77360145f47fa
Yesica Fica Millan <ficamillan@gmail.com>
2024-01-20 20:17:31 -0300

    [m - v1] READMEh

---
+++ README.md
@@ -0,0 +1,13 @@
+# Juego del Número Secreto
+
+Bienvenido al Juego del Número Secreto, un proyecto desarrollado como parte del curso de Curso Lógica de programación: sumérgete en la programación con JavaScript - Alura. Este juego interactivo desafía a los usuarios a adivinar un número dentro de un rango específico.
+
+## Características
+
+- Generación aleatoria de un número secreto.
+- Retroalimentación en tiempo real sobre las conjeturas del usuario.
+- Límite de intentos para añadir un factor de desafío.
+
+## Demostración
+
+Puedes probar el juego [aquí](https://fica-millan.github.io/logicaDeProgramacion_Alura/).
--- app.js
+++ app.js
@@ -4,37 +4,31 @@ let numeroMaximoPosible = 100;
 let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
 let numeroUsuario = 0;
 let intentos = 1;
-//let palabraVeces = 'intento';
 let maximoIntentos = 6;
 
 //console.log(numeroSecreto);
 
 while (numeroUsuario != numeroSecreto){
     // el Prompt le solicita informacion al usuario
-    numeroUsuario = parseInt (prompt(`Me indicas un numero entre 1 y ${numeroMaximoPosible} por favor:`));
+    numeroUsuario = parseInt (prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible} por favor:`));
 
     // muestra en la consola el numero que ingresa el usuario
-    console.log('Numero seleccionado por el usuario', numeroUsuario);
+    console.log('Número seleccionado por el usuario', numeroUsuario);
     // verificar la comparación entre "numeroUsuario" y "numeroSecreto"
     console.log('Resultado de la comparación:', numeroUsuario == numeroSecreto);
 
-    // Se realiza la comparacion (el == es para comparar, el = es para asignar)
+    // Se realiza la comparacion
     if (numeroUsuario == numeroSecreto) {
-        // el Alert abre una ventana emergente 
-        //alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}.`);
-        alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}.`);
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'intento' : 'intentos'}.`);
     } else{
         if(numeroUsuario > numeroSecreto){
-            alert("El numero secreto es menor");
+            alert("El número secreto es menor.");
         }else{
-            alert("El numero secreto es mayor.");
+            alert("El número secreto es mayor.");
         }
-        //intentos = intentos + 1;
-        //intentos +=1;
         intentos ++;
-        //palabraVeces = 'intentos';
         if (intentos > maximoIntentos){
-            alert(`Llegaste al numero maximo de ${maximoIntentos} intentos`);
+            alert(`Llegaste al número máximo de ${maximoIntentos} intentos`);
             break;
         }
     }
